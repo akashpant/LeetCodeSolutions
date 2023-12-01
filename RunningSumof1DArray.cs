68ad8c44c898d2c6f7d92df76db0ba9b42ef1fc1
Akash Pant <64428395+akashpant@users.noreply.github.com>
2023-12-01 07:52:44 +0530

    Create RunningSumof1DArray.cs

---
+++ RunningSumof1DArray.cs
@@ -0,0 +1,24 @@
+/*
+Intuition
+You will observe the next element is simply the sum of its preceding element and it self.
+
+Approach
+As the original array is not needed, updating each index as sum of previous and current index will provide the required array.
+
+Complexity
+Time complexity:
+O(n)
+Space complexity:
+O(1)
+*/
+
+//Code
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+        return nums;
+    }
+}
